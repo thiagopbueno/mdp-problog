88e0ba9584a81202762a7bbc741f674149d69039
Thiago P Bueno <thiago.pbueno@gmail.com>
2016-03-10 10:32:43 -0300

    Break model string into subparts

--- sysadmin.py
+++ sysadmin.py
@@ -3,24 +3,35 @@
 from problog.tasks.dtproblog import dtproblog
 from problog.program import PrologString
 
-model = """
+objects = """
 computer(c1).
 computer(c3).
 computer(c2).
+"""
 
-?::reboot(X) :- computer(X), X \== c0.
+decision_facts = """
+?::reboot(X) :- computer(X).
+"""
 
-utility(reboot(X), -0.75) :- computer(X), X \== c0.
-utility(running(X), 1.00) :- computer(X), X \== c0.
+utility_attributes = """
+utility(reboot(X), -0.75) :- computer(X).
+utility(running(X), 1.00) :- computer(X).
+"""
 
+background_knowledge = """
 1.00::running(X) :- reboot(X).
 0.10::running(X) :- not(reboot(X)), not(was_running(X)).
 1.00::running(X) :- not(reboot(X)), was_running(X), was_running(Y), was_running(Z), Y \== Z.
 0.83::running(X) :- not(reboot(X)), was_running(X), not(was_running(Y)), was_running(Z), Y \== Z.
 0.56::running(X) :- not(reboot(X)), was_running(X), not(was_running(Y)), not(was_running(Z)), Y \== Z.
-
-was_running(c0).
 """
+
+initial_state = """
+was_running(fake).
+was_running(c1).
+"""
+
+model = objects + decision_facts + utility_attributes + background_knowledge + initial_state
 
 program = PrologString(model)
 decisions, score, statistics = dtproblog(program)
