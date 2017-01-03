1b534d3329e7d48ac4b07eeb34fc528e86fb6f7e
Thiago P. Bueno <thiago.pbueno@gmail.com>
2017-01-03 15:03:22 -0200

    Add __prepare() method to MDP class in order to get self._engine ready to compute transitions

--- mdpproblog/mdp.py
+++ mdpproblog/mdp.py
@@ -31,6 +31,24 @@ class MDP(object):
 		self._model = model
 		self._engine = eng.Engine(model)
 
+		self.__prepare()
+
+	def __prepare(self):
+		engine = self._engine
+
+		for term in engine.declarations('state_fluent'):
+			engine.add_fact(Fluent.create_fluent(term, 0), 0.5)
+
+		actions = engine.declarations('action')
+		engine.add_annotated_disjunction(actions, [1.0/len(actions)]*len(actions))
+
+		utilities = engine.assignments('utility')
+		next_state_fluents = [Fluent.create_fluent(f, 1) for f in self.state_fluents()]
+		queries = list(set(utilities) | set(next_state_fluents) | set(actions))
+
+		engine.relevant_ground(queries)
+		self.__queries = engine.compile(self.next_state_fluents())
+
 	def state_fluents(self):
 		"""
 		Return an ordered list of state fluent objects.
